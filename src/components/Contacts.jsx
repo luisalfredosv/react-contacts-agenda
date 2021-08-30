deef389d78f0a46339a3617f4190fcb76aed57f2
luisalfredosv <luissarabia80@gmail.com>
2021-08-30 17:02:06 -0400

    Update Contacts.jsx

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,37 +1,25 @@
 import React from "react";
+import TableContacts from "./TableContacts";
 
 const Contacts = () => {
+	const contacts = () => [
+		{
+			id: "raondoand",
+			name: "Jhon",
+			lastName: "Doe",
+			phoneNumber: "65661646",
+		},
+		{
+			id: "raondoand",
+			name: "Jhon",
+			lastName: "Doe",
+			phoneNumber: "65661646",
+		},
+	];
+
 	return (
 		<div className='container mt-3'>
-			<table class='table'>
-				<thead>
-					<tr>
-						<th scope='col'>#</th>
-						<th scope='col'>First</th>
-						<th scope='col'>Last</th>
-						<th scope='col'>Handle</th>
-					</tr>
-				</thead>
-				<tbody>
-					<tr>
-						<th scope='row'>1</th>
-						<td>Mark</td>
-						<td>Otto</td>
-						<td>@mdo</td>
-					</tr>
-					<tr>
-						<th scope='row'>2</th>
-						<td>Jacob</td>
-						<td>Thornton</td>
-						<td>@fat</td>
-					</tr>
-					<tr>
-						<th scope='row'>3</th>
-						<td colspan='2'>Larry the Bird</td>
-						<td>@twitter</td>
-					</tr>
-				</tbody>
-			</table>
+			<TableContacts contacts={contacts} />
 		</div>
 	);
 };
