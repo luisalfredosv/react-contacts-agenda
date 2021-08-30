12ca9c625f36cfc3b6f8606ad5d7a1b6b3a86cc9
luisalfredosv <luissarabia80@gmail.com>
2021-08-30 16:12:06 -0400

    [Create] Header Component

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+	return (
+		<nav className='navbar navbar-dark bg-dark'>
+			<div className='container-fluid'>
+				<h1 className='navbar-brand'>
+					Contacts agenda React + Reducer
+				</h1>
+			</div>
+		</nav>
+	);
+};
+
+export default Header;
