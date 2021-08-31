ffd1465c0ab80d4da6c5895e50b2ccf08b006faa
luisalfredosv <luissarabia80@gmail.com>
2021-08-31 17:03:35 -0400

    [Add] ContactsReducer.js

---
+++ src/reducers/ContactsReducer.js
@@ -0,0 +1,10 @@
+export const ContactsReducer = (state = [], action) => {
+	switch (action.type) {
+		case "add":
+			return [...state, action.payload];
+		case "delete":
+			return state.filter(({ id }) => id !== action.payload);
+		default:
+			return state;
+	}
+};
