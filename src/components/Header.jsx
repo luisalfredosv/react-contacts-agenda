21feb62f600adad7647cfdbecd66784927d36056
luisalfredosv <luissarabia80@gmail.com>
2021-08-31 17:03:25 -0400

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,13 +2,15 @@ import React from "react";
 
 const Header = () => {
 	return (
-		<nav className='navbar navbar-dark bg-dark'>
-			<div className='container-fluid'>
-				<h1 className='navbar-brand'>
-					Contacts agenda React + Reducer
-				</h1>
-			</div>
-		</nav>
+		<>
+			<nav className='navbar navbar-dark bg-dark'>
+				<div className='container-fluid'>
+					<h1 className='navbar-brand'>
+						Contacts agenda React + Reducer
+					</h1>
+				</div>
+			</nav>
+		</>
 	);
 };
 
