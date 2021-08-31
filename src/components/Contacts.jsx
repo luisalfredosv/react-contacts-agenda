423318d52136a35619b77af846339bd54159b71e
luisalfredosv <luissarabia80@gmail.com>
2021-08-31 17:03:20 -0400

    Update Contacts.jsx

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,26 +1,44 @@
-import React from "react";
+import React, { useReducer, useEffect, useState } from "react";
 import TableContacts from "./TableContacts";
+import FormContact from "./FormContact";
+import { ContactsReducer } from "../reducers/ContactsReducer";
+
+const init = () => {
+	const contacts = localStorage.getItem("contacts");
+	return contacts ? JSON.parse(contacts) : [];
+};
 
 const Contacts = () => {
-	const contacts = () => [
-		{
-			id: "raondoand",
-			name: "Jhon",
-			lastName: "Doe",
-			phoneNumber: "65661646",
-		},
-		{
-			id: "raondoand",
-			name: "Jhon",
-			lastName: "Doe",
-			phoneNumber: "65661646",
-		},
-	];
+	const [state, dispatch] = useReducer(ContactsReducer, [], init);
+
+	useEffect(() => {
+		localStorage.setItem("contacts", JSON.stringify(state));
+	}, [state]);
+
+	const [formView, setFormView] = useState(false);
 
 	return (
-		<div className='container mt-3'>
-			<TableContacts contacts={contacts} />
-		</div>
+		<>
+			<div className='container mt-3'>
+				<button
+					className={
+						!formView
+							? "btn btn-sm btn-success"
+							: "btn btn-sm btn-secondary"
+					}
+					onClick={() => setFormView(!formView)}
+				>
+					{!formView ? "Add Contact" : "Close form"}
+				</button>
+				{formView && (
+					<div>
+						<FormContact dispatch={dispatch} />
+					</div>
+				)}
+
+				<TableContacts contacts={state} dispatch={dispatch} />
+			</div>
+		</>
 	);
 };
 
